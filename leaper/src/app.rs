ee8c21184f38a2e374336ae8d333df519cdba812
Vitalii Lukyanov <tukanoid@outlook.com>
2025-06-20 11:12:12 +0200

    refactor before i start implementing other stuff

--- leaper/src/app.rs
+++ leaper/src/app.rs
@@ -1,34 +1,29 @@
+mod apps;
+
 use std::sync::Arc;
 
 use directories::ProjectDirs;
 use iced::{
-    Event, Length,
-    advanced::widget::{Id, operate, operation::scrollable::scroll_to},
-    alignment::{Horizontal, Vertical},
+    Event,
     keyboard::{self, Key, key},
-    widget::{button, center, column, horizontal_rule, image, row, scrollable, text, text_input},
+    widget::text_input,
 };
-use iced_aw::Spinner;
 use iced_layershell::Application;
-use itertools::Itertools;
-use leaper_apps::{AppEntry, AppsResult, search_apps};
+use leaper_apps::AppEntry;
 use leaper_db::{DB, DBResult};
 use tracing::Instrument;
+
+use crate::app::apps::{Apps, AppsMsg};
 
 pub type AppTheme = iced::Theme;
 pub type AppRenderer = iced::Renderer;
 pub type AppElement<'a> = iced::Element<'a, AppMsg, AppTheme, AppRenderer>;
-pub type AppTask = iced::Task<AppMsg>;
+pub type AppTask<Msg = AppMsg> = iced::Task<Msg>;
 
 pub struct App {
     db: Option<Arc<DB>>,
 
-    apps: AppsIcons,
-    filtered: AppsIcons,
-
-    search: String,
-    matcher: nucleo::Matcher,
-    selected: usize,
+    apps: Apps,
 }
 
 impl Application for App {
@@ -42,16 +37,10 @@ impl Application for App {
 
         let res = Self {
             db: None,
-
-            apps: vec![],
-            filtered: vec![],
-
-            search: String::new(),
-            matcher: nucleo::Matcher::new(nucleo::Config::DEFAULT),
-            selected: 0,
+            apps: Default::default(),
         };
         let task = AppTask::batch([
-            text_input::focus(Self::SEARCH_ID),
+            text_input::focus(Apps::SEARCH_ID),
             AppTask::perform(DB::init(db_path), |db| AppMsg::InitDB(db.map(Arc::new))),
         ]);
 
@@ -73,132 +62,17 @@ impl Application for App {
                             let span = tracing::trace_span!("get_cached_list");
                             async move { db.get_table::<AppEntry>().await }.instrument(span)
                         },
-                        AppMsg::InitApps,
-                    );
+                        AppsMsg::InitApps,
+                    )
+                    .map(Into::into);
                 }
                 Err(err) => {
                     tracing::error!("Failed to initialize the database: {err}");
                     return iced::exit();
                 }
             },
-            AppMsg::InitApps(apps) => match apps {
-                Ok(apps) => {
-                    self.apps = apps;
-                    self.apps.sort_by_key(|a| a.name.clone());
 
-                    tracing::trace!(
-                        "Initialized apps list from cache [{} entries]",
-                        self.apps.len()
-                    );
-
-                    return AppTask::perform(
-                        search_apps(self.db.clone().unwrap()),
-                        AppMsg::LoadApps,
-                    );
-                }
-                Err(err) => {
-                    tracing::error!("Failed to initialize app list from cache: {err}");
-                    return iced::exit();
-                }
-            },
-            AppMsg::LoadApps(apps) => match apps {
-                Ok(apps) => {
-                    self.apps = apps;
-                    self.selected = self.selected.clamp(0, self.apps.len() - 1);
-
-                    tracing::trace!("Loaded a fresh list of apps [{} entries]", self.apps.len());
-                }
-                Err(err) => {
-                    tracing::error!("Failed to load new app list: {err}");
-                    return iced::exit();
-                }
-            },
-
-            AppMsg::SearchInput(new_search) => {
-                self.search = new_search;
-
-                self.filtered = match self.search.as_str() {
-                    "" => {
-                        self.selected = match self.apps.len() {
-                            0 => 0,
-                            len => self.selected.clamp(0, len - 1),
-                        };
-
-                        vec![]
-                    }
-                    search => {
-                        self.selected = match self.filtered.len() {
-                            0 => 0,
-                            len => self.selected.clamp(0, len - 1),
-                        };
-
-                        self.apps
-                            .iter()
-                            .filter_map(|app| {
-                                self.matcher
-                                    .fuzzy_match(
-                                        nucleo::Utf32Str::new(&app.name, &mut vec![]),
-                                        nucleo::Utf32Str::new(&search.to_lowercase(), &mut vec![]),
-                                    )
-                                    .map(|score| (score, app))
-                            })
-                            .sorted_by_key(|(score, _)| *score)
-                            .rev()
-                            .map(|(_, app)| app.clone())
-                            .collect()
-                    }
-                };
-            }
-
-            AppMsg::RunSelectedApp => match self.apps.is_empty() {
-                true => {}
-                false => return AppTask::done(AppMsg::RunApp(self.selected)),
-            },
-            AppMsg::RunApp(ind) => match {
-                match self.search.is_empty() {
-                    true => &self.apps,
-                    false => &self.filtered,
-                }
-            }
-            .get(ind)
-            {
-                Some(app) => {
-                    tracing::trace!("Running {}: {:?}", app.name, app.exec);
-
-                    let cmd = &app.exec[0];
-                    let args = match app.exec.len() {
-                        1 => None,
-                        _ => Some(app.exec[1..].iter()),
-                    };
-
-                    let mut cmd = std::process::Command::new(cmd);
-
-                    if let Some(args) = args {
-                        cmd.args(args);
-                    }
-
-                    if let Err(err) = cmd.spawn() {
-                        tracing::error!("Failed to run the app {}: {err}", app.name)
-                    }
-
-                    return iced::exit();
-                }
-                None => tracing::warn!("Logic error!"),
-            },
-            AppMsg::ScrollToSelected => match self.apps.is_empty() {
-                true => {}
-                false => {
-                    let y_offset = self.selected as f32 * Self::APP_ENTRY_HEIGHT;
-
-                    return operate(scroll_to(
-                        Id::new(Self::LIST_ID),
-                        scrollable::AbsoluteOffset {
-                            x: 0.0,
-                            y: y_offset,
-                        },
-                    ));
-                }
-            },
+            AppMsg::Apps(apps_msg) => return self.apps.update(apps_msg, &self.db),
 
             AppMsg::IcedEvent(ev) => {
                 if let Event::Keyboard(event) = ev
@@ -210,29 +84,13 @@ impl Application for App {
                         }
 
                         Key::Named(key::Named::ArrowUp) => {
-                            self.selected = match self.apps.is_empty() {
-                                true => 0,
-                                false => match self.selected {
-                                    0 => self.apps.len() - 1,
-                                    x => x - 1,
-                                },
-                            };
-
-                            return AppTask::done(AppMsg::ScrollToSelected);
+                            return AppTask::done(AppsMsg::SelectUp.into());
                         }
                         Key::Named(key::Named::ArrowDown) => {
-                            self.selected = match self.apps.is_empty() {
-                                true => 0,
-                                false => match self.selected >= self.apps.len() - 1 {
-                                    true => 0,
-                                    false => self.selected + 1,
-                                },
-                            };
-
-                            return AppTask::done(AppMsg::ScrollToSelected);
+                            return AppTask::done(AppsMsg::SelectDown.into());
                         }
                         Key::Named(key::Named::Enter) => {
-                            return AppTask::done(AppMsg::RunSelectedApp);
+                            return AppTask::done(AppsMsg::RunSelectedApp.into());
                         }
 
                         _ => {}
@@ -253,12 +111,7 @@ impl Application for App {
     }
 
     fn view(&self) -> AppElement<'_> {
-        column![self.search(), horizontal_rule(2), self.list()]
-            .width(Length::Fill)
-            .height(Length::Fill)
-            .padding(20)
-            .spacing(10)
-            .into()
+        self.apps.view().map(Into::into)
     }
 
     fn theme(&self) -> Self::Theme {
@@ -270,128 +123,30 @@ impl Application for App {
     }
 }
 
-impl App {
-    const SEARCH_ID: &'static str = "search_input";
-    const LIST_ID: &'static str = "list";
-
-    fn search(&self) -> AppElement<'_> {
-        center(
-            text_input("Search for an app...", &self.search)
-                .id(text_input::Id::new(Self::SEARCH_ID))
-                .on_input_maybe((!self.apps.is_empty()).then_some(AppMsg::SearchInput))
-                .on_submit(AppMsg::RunSelectedApp)
-                .size(25)
-                .padding(10),
-        )
-        .width(Length::Fill)
-        .height(Length::Shrink)
-        .padding(10)
-        .into()
-    }
-
-    fn list(&self) -> AppElement<'_> {
-        let (items, filtered) = match self.search.is_empty() {
-            true => (&self.apps, false),
-            false => (&self.filtered, true),
-        };
-
-        let scrllbl = || {
-            scrollable(
-                column(
-                    items
-                        .iter()
-                        .enumerate()
-                        .map(|(ind, app)| Self::app_entry(app, ind, self.selected)),
-                )
-                .align_x(Horizontal::Center),
-            )
-            .id(scrollable::Id::new(Self::LIST_ID))
-            .width(Length::Fill)
-            .height(Length::Fill)
-            .into()
-        };
-
-        match filtered {
-            true => match items.is_empty() {
-                true => center(text("No matches found!").size(25)).into(),
-                false => scrllbl(),
-            },
-            false => match items.is_empty() {
-                true => center(
-                    row![
-                        Spinner::new().width(30).height(30),
-                        text("Loading...").size(20)
-                    ]
-                    .align_y(Vertical::Center)
-                    .spacing(10),
-                )
-                .into(),
-                false => scrllbl(),
-            },
-        }
-    }
-
-    const APP_ENTRY_HEIGHT: f32 = 50.0;
-    const APP_ENTRY_PADDING: [f32; 2] = [10.0, 5.0];
-    const APP_ENTRY_SPACING: f32 = 10.0;
-    const APP_ENTRY_IMAGE_SIZE: f32 = Self::APP_ENTRY_HEIGHT - Self::APP_ENTRY_PADDING[1] * 2.0;
-    const APP_ENTRY_TEXT_HEIGHT: f32 = Self::APP_ENTRY_IMAGE_SIZE * 0.5;
-
-    fn app_entry<'a>(app: &'a AppEntry, ind: usize, selected: usize) -> AppElement<'a> {
-        let r = match &app.icon {
-            Some(icon) => row![
-                image(&icon.path)
-                    .width(Self::APP_ENTRY_IMAGE_SIZE)
-                    .height(Self::APP_ENTRY_IMAGE_SIZE)
-            ],
-            None => row![],
-        }
-        .push(text(&app.name).size(Self::APP_ENTRY_TEXT_HEIGHT))
-        .height(Length::Fill)
-        .spacing(Self::APP_ENTRY_SPACING)
-        .align_y(Vertical::Center);
-
-        button(
-            center(r)
-                .width(Length::Fill)
-                .padding(Self::APP_ENTRY_PADDING),
-        )
-        .on_press(AppMsg::RunApp(ind))
-        .style(move |theme, status| {
-            let status = match selected == ind {
-                true => button::Status::Hovered,
-                false => status,
-            };
-
-            button::secondary(theme, status)
-        })
-        .height(Length::Fixed(Self::APP_ENTRY_HEIGHT))
-        .width(Length::Fill)
-        .into()
-    }
-}
-
 pub struct AppFlags {
     pub project_dirs: ProjectDirs,
 }
-
-type AppsIcons = Vec<AppEntry>;
-
-type InitAppsIconsResult = DBResult<AppsIcons>;
-type LoadAppsIconsResult = AppsResult<AppsIcons>;
 
 #[iced_layershell::to_layer_message]
 #[derive(Debug, Clone)]
 pub enum AppMsg {
     InitDB(DBResult<Arc<DB>>),
-    InitApps(InitAppsIconsResult),
-    LoadApps(LoadAppsIconsResult),
 
-    SearchInput(String),
-
-    RunSelectedApp,
-    RunApp(usize),
-    ScrollToSelected,
+    Apps(AppsMsg),
 
     IcedEvent(Event),
 }
+
+macro_rules! into_app_msg {
+    ($($name:ident($err:ty)),+) => {
+        $(
+            impl From<$err> for AppMsg {
+                fn from(val: $err) -> Self {
+                    Self::$name(val)
+                }
+            }
+        )+
+    };
+}
+
+into_app_msg![Apps(AppsMsg)];
---
+++ leaper/src/app/apps.rs
@@ -0,0 +1,303 @@
+use std::sync::Arc;
+
+use iced::{
+    Length,
+    advanced::widget::{Id, operate, operation::scrollable::scroll_to},
+    alignment::{Horizontal, Vertical},
+    widget::{button, center, column, horizontal_rule, image, row, scrollable, text, text_input},
+};
+use iced_aw::Spinner;
+use itertools::Itertools;
+use leaper_apps::{AppEntry, AppsResult, search_apps};
+use leaper_db::{DB, DBResult};
+
+use crate::app::{AppElement, AppTask};
+
+type AppsIcons = Vec<AppEntry>;
+
+type InitAppsIconsResult = DBResult<AppsIcons>;
+type LoadAppsIconsResult = AppsResult<AppsIcons>;
+
+#[derive(Default)]
+pub struct Apps {
+    apps: AppsIcons,
+    filtered: AppsIcons,
+
+    search: String,
+    matcher: nucleo::Matcher,
+    selected: usize,
+}
+
+impl Apps {
+    pub fn update(&mut self, msg: AppsMsg, db: &Option<Arc<DB>>) -> AppTask {
+        match msg {
+            AppsMsg::InitApps(apps) => match apps {
+                Ok(apps) => {
+                    self.apps = apps;
+                    self.apps.sort_by_key(|a| a.name.clone());
+
+                    tracing::trace!(
+                        "Initialized apps list from cache [{} entries]",
+                        self.apps.len()
+                    );
+
+                    return AppTask::perform(search_apps(db.clone().unwrap()), AppsMsg::LoadApps)
+                        .map(Into::into);
+                }
+                Err(err) => {
+                    tracing::error!("Failed to initialize app list from cache: {err}");
+                    return iced::exit();
+                }
+            },
+            AppsMsg::LoadApps(apps) => match apps {
+                Ok(apps) => {
+                    self.apps = apps;
+                    self.selected = self.selected.clamp(0, self.apps.len() - 1);
+
+                    tracing::trace!("Loaded a fresh list of apps [{} entries]", self.apps.len());
+                }
+                Err(err) => {
+                    tracing::error!("Failed to load new app list: {err}");
+                    return iced::exit();
+                }
+            },
+
+            AppsMsg::SearchInput(new_search) => {
+                self.search = new_search;
+
+                self.filtered = match self.search.as_str() {
+                    "" => {
+                        self.selected = match self.apps.len() {
+                            0 => 0,
+                            len => self.selected.clamp(0, len - 1),
+                        };
+
+                        vec![]
+                    }
+                    search => {
+                        self.selected = match self.filtered.len() {
+                            0 => 0,
+                            len => self.selected.clamp(0, len - 1),
+                        };
+
+                        self.apps
+                            .iter()
+                            .filter_map(|app| {
+                                self.matcher
+                                    .fuzzy_match(
+                                        nucleo::Utf32Str::new(&app.name, &mut vec![]),
+                                        nucleo::Utf32Str::new(&search.to_lowercase(), &mut vec![]),
+                                    )
+                                    .map(|score| (score, app))
+                            })
+                            .sorted_by_key(|(score, _)| *score)
+                            .rev()
+                            .map(|(_, app)| app.clone())
+                            .collect()
+                    }
+                };
+            }
+
+            AppsMsg::SelectUp => {
+                self.selected = match self.apps.is_empty() {
+                    true => 0,
+                    false => match self.selected {
+                        0 => self.apps.len() - 1,
+                        x => x - 1,
+                    },
+                };
+
+                return AppTask::done(AppsMsg::ScrollToSelected).map(Into::into);
+            }
+            AppsMsg::SelectDown => {
+                self.selected = match self.apps.is_empty() {
+                    true => 0,
+                    false => match self.selected >= self.apps.len() - 1 {
+                        true => 0,
+                        false => self.selected + 1,
+                    },
+                };
+
+                return AppTask::done(AppsMsg::ScrollToSelected).map(Into::into);
+            }
+
+            AppsMsg::RunSelectedApp => match self.apps.is_empty() {
+                true => {}
+                false => return AppTask::done(AppsMsg::RunApp(self.selected)).map(Into::into),
+            },
+            AppsMsg::RunApp(ind) => match {
+                match self.search.is_empty() {
+                    true => &self.apps,
+                    false => &self.filtered,
+                }
+            }
+            .get(ind)
+            {
+                Some(app) => {
+                    tracing::trace!("Running {}: {:?}", app.name, app.exec);
+
+                    let cmd = &app.exec[0];
+                    let args = match app.exec.len() {
+                        1 => None,
+                        _ => Some(app.exec[1..].iter()),
+                    };
+
+                    let mut cmd = std::process::Command::new(cmd);
+
+                    if let Some(args) = args {
+                        cmd.args(args);
+                    }
+
+                    if let Err(err) = cmd.spawn() {
+                        tracing::error!("Failed to run the app {}: {err}", app.name)
+                    }
+
+                    return iced::exit();
+                }
+                None => tracing::warn!("Logic error!"),
+            },
+
+            AppsMsg::ScrollToSelected => match self.apps.is_empty() {
+                true => {}
+                false => {
+                    let y_offset = self.selected as f32 * Self::APP_ENTRY_HEIGHT;
+
+                    return operate(scroll_to(
+                        Id::new(Self::LIST_ID),
+                        scrollable::AbsoluteOffset {
+                            x: 0.0,
+                            y: y_offset,
+                        },
+                    ));
+                }
+            },
+        }
+
+        AppTask::none()
+    }
+
+    pub fn view(&self) -> AppElement<'_> {
+        column![self.search(), horizontal_rule(2), self.list()]
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .padding(20)
+            .spacing(10)
+            .into()
+    }
+
+    pub const SEARCH_ID: &'static str = "app_search_input";
+    const LIST_ID: &'static str = "list";
+
+    fn search(&self) -> AppElement<'_> {
+        center(
+            text_input("Search for an app...", &self.search)
+                .id(text_input::Id::new(Self::SEARCH_ID))
+                .on_input_maybe(
+                    (!self.apps.is_empty()).then_some(|s| AppsMsg::SearchInput(s).into()),
+                )
+                .on_submit(AppsMsg::RunSelectedApp.into())
+                .size(25)
+                .padding(10),
+        )
+        .width(Length::Fill)
+        .height(Length::Shrink)
+        .padding(10)
+        .into()
+    }
+
+    fn list(&self) -> AppElement<'_> {
+        let (items, filtered) = match self.search.is_empty() {
+            true => (&self.apps, false),
+            false => (&self.filtered, true),
+        };
+
+        let scrllbl = || {
+            scrollable(
+                column(
+                    items
+                        .iter()
+                        .enumerate()
+                        .map(|(ind, app)| Self::app_entry(app, ind, self.selected)),
+                )
+                .align_x(Horizontal::Center),
+            )
+            .id(scrollable::Id::new(Self::LIST_ID))
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .into()
+        };
+
+        match filtered {
+            true => match items.is_empty() {
+                true => center(text("No matches found!").size(25)).into(),
+                false => scrllbl(),
+            },
+            false => match items.is_empty() {
+                true => center(
+                    row![
+                        Spinner::new().width(30).height(30),
+                        text("Loading...").size(20)
+                    ]
+                    .align_y(Vertical::Center)
+                    .spacing(10),
+                )
+                .into(),
+                false => scrllbl(),
+            },
+        }
+    }
+
+    const APP_ENTRY_HEIGHT: f32 = 50.0;
+    const APP_ENTRY_PADDING: [f32; 2] = [10.0, 5.0];
+    const APP_ENTRY_SPACING: f32 = 10.0;
+    const APP_ENTRY_IMAGE_SIZE: f32 = Self::APP_ENTRY_HEIGHT - Self::APP_ENTRY_PADDING[1] * 2.0;
+    const APP_ENTRY_TEXT_HEIGHT: f32 = Self::APP_ENTRY_IMAGE_SIZE * 0.5;
+
+    fn app_entry<'a>(app: &'a AppEntry, ind: usize, selected: usize) -> AppElement<'a> {
+        let r = match &app.icon {
+            Some(icon) => row![
+                image(&icon.path)
+                    .width(Self::APP_ENTRY_IMAGE_SIZE)
+                    .height(Self::APP_ENTRY_IMAGE_SIZE)
+            ],
+            None => row![],
+        }
+        .push(text(&app.name).size(Self::APP_ENTRY_TEXT_HEIGHT))
+        .height(Length::Fill)
+        .spacing(Self::APP_ENTRY_SPACING)
+        .align_y(Vertical::Center);
+
+        button(
+            center(r)
+                .width(Length::Fill)
+                .padding(Self::APP_ENTRY_PADDING),
+        )
+        .on_press(AppsMsg::RunApp(ind).into())
+        .style(move |theme, status| {
+            let status = match selected == ind {
+                true => button::Status::Hovered,
+                false => status,
+            };
+
+            button::secondary(theme, status)
+        })
+        .height(Length::Fixed(Self::APP_ENTRY_HEIGHT))
+        .width(Length::Fill)
+        .into()
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum AppsMsg {
+    InitApps(InitAppsIconsResult),
+    LoadApps(LoadAppsIconsResult),
+
+    SearchInput(String),
+
+    SelectUp,
+    SelectDown,
+
+    RunSelectedApp,
+    RunApp(usize),
+    ScrollToSelected,
+}
